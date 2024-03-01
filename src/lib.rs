96e5491f974a65f06da63f2c237d3040a3dbf37e
Shane Celis <shane.celis@gmail.com>
2024-03-01 04:23:57 -0500

    doc: Introduce the lib.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+//! Iterators come in many varieties:
+//!
+//! - fused
+//! - unfused
+//! - frayed
+//!
+//! # Fused
+//!
+//! A fused iterator once `.next()` returns `None` will only ever return `None`.
+//! It is exhausted at the first `None`. This behavior can be guaranteed with
+//! the `.fuse()` method that returns a [std::iter::Fuse] iterator.
+//!
+//! # Unfused
+//!
+//! The majority of iterators are unfused. They have no programmatic guarantees
+//! like [Fuse] but the producers and consumers have tacitly agreed that it's
+//! impolite to ask `.next()` of an iterator who has already said `None`.
+//!
+//! # Frayed
+//!
+//! Frayed iterators delight in returning elements after their first, second, or
+//! even third `None`. They can economically be used to represent many
+//! sequences. We are not barbarians, however, so we consider a frayed iterator
+//! to be exhausted when it returns two `None`s consecutively.
+//!
 mod prefix_unfused;
 mod split_unfused;
 use crate::prefix_unfused::UnfusedPrefix;
